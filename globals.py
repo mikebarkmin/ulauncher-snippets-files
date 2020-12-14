a86506bbd7397f3983d92db51cefb3109f6e854e
Mike Barkmin <mike@barkmin.eu>
2020-12-14 11:55:17 +0100

    improve journal template

--- globals.py
+++ globals.py
@@ -1,12 +1,11 @@
-
 import json
 import requests
+
 
 def quote():
     res = requests.get("http://quotes.rest/qod.json?category=inspire").json()
     quote = res["contents"]["quotes"][0]
-    return quote["quote"] + "\\n -- " + quote["author"]
+    return f"{quote['quote']} -- [[{quote['author']}]]"
 
-globals = {
-    "quote": quote
-}
+
+globals = {"quote": quote}
